820ebed426f43c27168d1cce725e5e5d91296b18
Gustavo Morais <gmoraisildefonso@gmail.com>
2025-04-16 18:35:32 -0300

    Mapeando dados e utilizando generics

--- src/main/java/br/com/screenmatch/screenmatchv2/Screenmatchv2Application.java
+++ src/main/java/br/com/screenmatch/screenmatchv2/Screenmatchv2Application.java
@@ -1,6 +1,8 @@
 package br.com.screenmatch.screenmatchv2;
 
+import br.com.screenmatch.screenmatchv2.models.DadosSerie;
 import br.com.screenmatch.screenmatchv2.services.ConsumoAPI;
+import br.com.screenmatch.screenmatchv2.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +18,10 @@ public class Screenmatchv2Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoapi = new ConsumoAPI();
-		var json = consumoapi.obterDados("http://www.omdbapi.com/?t=Stranger+Things&Season=4&apikey=3333ecf7");
+		var json = consumoapi.obterDados("http://www.omdbapi.com/?t=Stranger+Things&apikey=3333ecf7");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
--- src/main/java/br/com/screenmatch/screenmatchv2/models/DadosSerie.java
+++ src/main/java/br/com/screenmatch/screenmatchv2/models/DadosSerie.java
@@ -1,7 +1,9 @@
 package br.com.screenmatch.screenmatchv2.models;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(
         @JsonAlias("Title") String titulo,
         @JsonAlias("totalSeasons") int totalTemporadas,
